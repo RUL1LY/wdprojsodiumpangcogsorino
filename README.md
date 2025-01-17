8e7bbc25f17cee7098f7e090f53e07386bf126a2
RUL1LY <sc180783@gmail.com>
2025-01-17 14:29:56 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,36 @@
+Title: Spill The Deej
+
+Description:
+A website with a stream of messages from a bot.
+This bot can also take in messages and display them on screen.
+Certain messages given to the bot can trigger changes in the website revealing new information
+about the bot and where the messages are from.
+There are also two archive webpages. One for everyone and one for those with a password.
+These webpages will show the previous messages that have been put in the system.
+
+Outline:
+
+- Homepage: On the left side of the screen, there will be an image of the confession bot.
+  Next to it will be the send messages button. On the right side will be the stream of messages.
+- Archives: This webpage will contain a lot of messages which have been the past messages in the system.
+  It will have a pagination section in the bottom to select which confessions you will see in the middle.
+- Admin Archives: This webpage will require a password to enter. When this password is entered, the user will be directed to a webpage very similar to the archives.
+  It will have a pagination section in the bottom to select what confessions you will see as the boxes above change to show the confessions.
+
+JS Description:
+
+- In the homepage, javascript will be used to cycle through the messages in the right hand side of the screen.
+  It will also be used to change the attributes of the elements in the pages to make the website responsive
+  such as hiding or showing new elements when certain actions are done.
+
+- In the archives, javascript will be used for the pagination and the changing of the confessions shown on screen.
+  Javascript will also be used to stop the user from going to this page after a certain event happens.
+
+- In the admin archives, javascript will be used to give access to the webpage if the password is given.
+  It will also be used to change parts of the webpage after certain actions are done like clicking certain areas.
+
+Wireframes (created using wireframe.cc):
+
+- https://wireframe.cc/aku8UR (Homepage)
+- https://wireframe.cc/onn2qT (Archives)
+- https://wireframe.cc/GpSjAE (Admin Archives)
---
+++ public/script.js
@@ -0,0 +1,252 @@
+//functions to get minutes to year in increasing order
+const date = new Date();
+
+function getMinute() {
+  return date.getMinutes() > 9 ? date.getMinutes() : `0${date.getMinutes()}`;
+}
+
+function getHour() {
+  return date.getHours() <= 12 ? date.getHours() : date.getHours() - 12;
+}
+
+function getAmPm() {
+  let h = date.getHours()
+  return h < 12 ? "am" : "pm";
+}
+
+function getDate(){
+  return date.getDate();
+}
+
+function getMonth() {
+  let month = date.getMonth() + 1;
+  
+  switch(month) {
+  case 1: month = "January"
+    break;
+  case 2: month = "February"
+    break;
+  case 3: month = "March"
+    break;
+  case 4: month = "April"
+    break;
+  case 5: month = "May"
+    break;
+  case 6: month = "June"
+    break;
+  case 7: month = "July"
+    break;
+  case 8: month = "August"
+    break;
+  case 9: month = "September"
+    break;
+  case 10: month = "October"
+    break;
+  case 11: month = "November"
+    break;
+  case 12: month = "December"
+    break;
+  default: alert("how did this happen?");
+  }
+  
+  return month;
+}
+
+function getYear() {
+  return date.getYear() + 1900;
+}
+
+
+//define the message objects
+let messages1 = {
+  number: null,
+  text: null,
+  date: null,
+  time: null,
+}
+
+let messages2 = {
+  number: null,
+  text: null,
+  date: null,
+  time: null,
+}
+
+let messages3 = {
+  number: null,
+  text: null,
+  date: null,
+  time: null,
+}
+
+let messages4 = {
+  number: null,
+  text: null,
+  date: null,
+  time: null,
+}
+
+let messages5 = {
+  number: null,
+  text: null,
+  date: null,
+  time: null,
+}
+
+//Displaying confessions
+let confessionNumber = 0;
+
+//update the confessions
+function updateConfessions() {
+  Object.assign(messages1, messages2);
+  Object.assign(messages2, messages3);
+  Object.assign(messages3, messages4);
+  Object.assign(messages4, messages5);  
+}
+
+function outputConfessions() {
+  if (messages1.number != null) document.getElementById("first").innerHTML = `Anonymous confession #${messages1.number} <br><br> ${messages1.text} <br><br> ${messages1.date}, at ${messages1.time}`;
+  if (messages2.number != null) document.getElementById("second").innerHTML = `Anonymous confession #${messages2.number} <br><br> ${messages2.text} <br><br> ${messages2.date}, at ${messages2.time}`;
+  if (messages3.number != null) document.getElementById("third").innerHTML = `Anonymous confession #${messages3.number} <br><br> ${messages3.text} <br><br> ${messages3.date}, at ${messages3.time}`;
+  if (messages4.number != null) document.getElementById("fourth").innerHTML = `Anonymous confession #${messages4.number} <br><br> ${messages4.text} <br><br> ${messages4.date}, at ${messages4.time}`;
+  if (messages5.number != null) document.getElementById("fifth").innerHTML = `Anonymous confession #${messages5.number} <br><br> ${messages5.text} <br><br> ${messages5.date}, at ${messages5.time}`;
+}
+
+//Bot responses
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+let arr = [];
+arr[0] = "What an interesting confession to make";
+arr[1] = "Are you sure you should be saying that?";
+arr[2] = "Wowww...";
+arr[3] = "Imagine I accidentally leaked who you are";
+arr[4] = "wha-";
+arr[5] = "I- I- I-";
+
+function botReply() {
+  //number for reply and number for delay
+  let replyNumber = Math.floor(Math.random() * 6);
+  let timeTaken = (Math.floor(Math.random() * 5) + 1) * 1000;
+  document.getElementById('responseText').innerHTML = "thinking..."
+  sleep(timeTaken).then(() => document.getElementById('responseText').innerHTML = arr[replyNumber]);
+}
+
+//input new confession and output
+
+function newConfession() {
+  event.preventDefault();
+  
+  updateConfessions();
+  
+  confessionNumber++;
+  messages5.number = confessionNumber;
+  messages5.text = document.getElementById('confession').value;
+  if (messages5.text.toLowerCase().toString() == "we love you sir we are sorry") {
+    document.getElementById('responseText').innerHTML = "Ah. So you know...";
+    document.getElementById('responseText').setAttribute("id", "bold");
+    sleep(5000).then(() => window.location.replace('firstEnding.html'));
+  } else {
+     messages5.date = `${getMonth()} ${getDate()} ${getYear()}`;
+    messages5.time = `${getHour()}:${getMinute()} ${getAmPm()}`;
+  
+    outputConfessions();
+  
+    botReply();
+  
+    document.getElementById('confession').value = '';
+  }
+ 
+}
+
+//Tutorial 
+let boxesDone = -1;
+
+document.addEventListener("DOMContentLoaded", function() {
+  const currPage = window.location.pathname;
+  if (currPage === "/mainPage.html") {
+    if (document.referrer.includes("firstEnding.html")) {
+      boxesDone = 6;
+    } else {
+      boxesDone = 2;
+      tutorial();
+    }
+  }
+});
+                          
+function tutorial() {
+  let saveNumber = confessionNumber;
+  boxesDone++;
+  switch (boxesDone) {
+    case 0:
+      document.getElementById('start').setAttribute("class", "hide")
+      document.getElementById('firstBox').removeAttribute("class");
+      break;
+    case 1:
+      document.getElementById('firstBox').setAttribute("class", "hide");
+      document.getElementById('secondBox').removeAttribute("class");
+      break;
+    case 2:
+      document.getElementById('secondBox').setAttribute("class", "hide");
+      sleep(3000).then(() => {
+        document.getElementById('thirdBox').removeAttribute("class");
+      });
+      break;
+    case 3:
+      document.getElementById('fourthBox').removeAttribute("class");
+      break;
+    case 4:
+      document.getElementById('fourthBox').setAttribute("class", "hide"); 
+      document.getElementById('fifthBox').removeAttribute("class");
+      break;
+    case 5:
+      document.getElementById('fifthBox').setAttribute("class", "hide"); 
+      document.getElementById('sixthBox').removeAttribute("class");
+      
+      waitForSubmit(saveNumber).then(() => {
+        sleep(5001).then(() => {
+          tutorialConfession();
+          sleep(1500).then(() => {
+            document.getElementById('sixthBox').setAttribute("class", "hide");
+            document.getElementById('seventhBox').removeAttribute("class");
+          });
+        });
+      });
+      break;
+    case 6:
+      document.getElementById('fourthBox').setAttribute("class", "hide");
+      console.log(6)
+      break;
+    default: console.log("How in the world did that happen?");
+  }
+}
+
+function waitForSubmit(saveNumber) {
+  return new Promise((resolve) => {
+    const interval = setInterval(() => {
+      console.log(`${confessionNumber}`)
+      if (confessionNumber != saveNumber) {
+        clearInterval(interval);
+        resolve();
+      }
+    }, 100)
+  })
+}
+
+function tutorialConfession() {
+  
+  updateConfessions();
+  
+  confessionNumber++;
+  messages5.number = confessionNumber;
+  messages5.text = "We, everyday, love, obey, value, empower. You offer urself so in retrospect, we eagerly await, ready, embrace, salute, ofc, Roy? Roy. You.";
+  messages5.date = `${getMonth()} ${getDate()} ${getYear()}`;
+  messages5.time = `${getHour()}:${getMinute()} ${getAmPm()}`;
+  
+  outputConfessions();
+  
+  document.getElementById('responseText').innerHTML = "Do you know?";
+  
+  document.getElementById('confession').value = '';
+}
